017f88ce57a95d11dbf7f2df51489e921c10a5dd
svitbka <svitbka@gmail.com>
2021-03-08 23:30:35 +0300

    first add

---
+++ index.js
@@ -0,0 +1,73 @@
+let switchPallette = document.getElementById('switchpallette');
+
+switchPallette.addEventListener('click', function() {
+    let pallette = document.getElementById('wrap__pallette');
+
+    if (pallette.hidden === false) {
+        pallette.hidden = true;
+    } else {
+        document.getElementById('pallette').style.cssText = `display: flex;`;
+        pallette.hidden = false;
+    }
+});
+
+document.getElementById('pallette').querySelectorAll('li').forEach(e => {
+    e.addEventListener('click', () => {
+        let color = document.getElementById('user__pallette');
+        let a = e.cloneNode(true);
+        a.style.cssText = `list-style: none;`;
+
+        let flag = true;
+        color.querySelectorAll('li').forEach(el => {
+            if (a.getAttribute('id') === el.getAttribute('id')) {
+                flag = false;
+            }
+        })
+
+        if (flag) {
+            color.append(a);
+        }
+
+    });
+});
+
+document.getElementById('button__reset').addEventListener('click', () => {
+    let userPallette = document.getElementById('user__pallette').querySelectorAll('li').forEach((e) => {
+        e.remove();
+    });
+});
+
+document.getElementById('button__save').addEventListener('click', () => {
+    let button = document.getElementById('button__save');
+
+    let pallette = document.getElementById('wrap__pallette');
+    pallette.hidden = true;
+});
+
+document.getElementById('button__draw').addEventListener('click', () => {
+    let colors = "";
+
+    document.getElementById('user__pallette').querySelectorAll('li').forEach((e) => {
+        colors += e.dataset.value;
+    });
+
+    let mapDataString = ganeration(),
+        parseData = parse(mapDataString),
+        normalizationData = dataNormalization(parseData),
+        lastData = makeStruct(normalizationData);
+
+    console.log(lastData);
+
+    let canvas = heatmapfromdata('canvas');
+
+    let dataColors = canvas.parseColors(colors),
+        gradientData = canvas.linearInterpolation(dataColors);
+    canvas.gradient(gradientData);
+
+    canvas.data(lastData);
+    canvas.draw();   
+});
+
+
+
+
